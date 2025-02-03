599e3200c29f46f1a2a7c4bcf2541a0203755c57
elOx <elox55555@gmail.com>
2025-02-03 03:01:22 +0200

    Add gradient to headline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,59 @@
-<h1>Hey, I'm Michał</h1>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let headline: HTMLHeadingElement;
+
+    onMount(() => {
+        setTimeout(() => {
+            headline.classList.remove("dim-left");
+        }, 500);
+
+        setTimeout(() => {
+            headline.classList.remove("dim-right");
+        }, 1000);
+    });
+</script>
+
+<h1 bind:this={headline} class="dim-left dim-right">Hey, I'm Michał</h1>
 
 <style>
+    @property --gradient-color-left {
+        syntax: "<color>";
+        initial-value: #77ff77;
+        inherits: false;
+    }
+
+    @property --gradient-color-right {
+        syntax: "<color>";
+        initial-value: #77ffff;
+        inherits: false;
+    }
+
     h1 {
-        color: #fff;
+        --gradient-color-left: #77ff77;
+        --gradient-color-right: #77ffff;
+
         text-transform: uppercase;
         font-size: 10rem;
+
+        background-image: linear-gradient(
+            to right,
+            var(--gradient-color-left),
+            var(--gradient-color-right)
+        );
+        background-clip: text;
+        color: transparent;
+
+        transition-property: --gradient-color-left, --gradient-color-right;
+        transition-duration: 1s;
+        transition-timing-function: ease-in-out;
+
+        &.dim-left {
+            --gradient-color-left: #223333;
+        }
+
+        &.dim-right {
+            --gradient-color-right: #333344;
+        }
     }
 </style>
