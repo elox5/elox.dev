637628f4cc1637c3a699a7116905ff9abf69a4f0
elOx <elox55555@gmail.com>
2025-02-03 02:44:26 +0200

    Add basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import "../app.css";
+
+    const { children } = $props();
+</script>
+
+<div class="app">
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/settings">Settings</a>
+    </nav>
+    <main>
+        {@render children()}
+    </main>
+</div>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    nav {
+        height: 100%;
+        width: 10rem;
+        flex-shrink: 0;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: start;
+
+        gap: 1rem;
+    }
+
+    main {
+        height: 100%;
+        flex-grow: 1;
+    }
+</style>
