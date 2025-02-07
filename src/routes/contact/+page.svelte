e2c4677ec1dad0111f694ca4a165433424e85e8d
elOx <elox55555@gmail.com>
2025-02-07 16:14:54 +0200

    Update contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,43 +1,50 @@
 <script lang="ts">
     import Fa from "svelte-fa";
-    import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
+    import {
+        faDiscord,
+        faGithub,
+        faItchIo,
+    } from "@fortawesome/free-brands-svg-icons";
     import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 </script>
 
-<h2>
-    <a href="https://github.com/elox5" target="_blank">
-        <Fa icon={faGithub} />
-        github.com/elox5
-    </a>
-</h2>
-<h2>
-    <a href="mailto:mrys5@proton.me">
-        <Fa icon={faEnvelope} />
-        mrys5@proton.me
-    </a>
-</h2>
-<h2>
-    <p>
-        <Fa icon={faDiscord} />
-        .elox
-    </p>
-</h2>
+<h2>Contact</h2>
+<a class="underline-hover" href="mailto:mrys5@proton.me">
+    <Fa icon={faEnvelope} />
+    mrys5@proton.me
+</a>
+<p>
+    <Fa icon={faDiscord} />
+    .elox
+</p>
+
+<h2>Links</h2>
+<a class="underline-hover" href="https://github.com/elox5" target="_blank">
+    <Fa icon={faGithub} />
+    github.com/elox5
+</a>
+<a class="underline-hover" href="https://el0x.itch.io" target="_blank">
+    <Fa icon={faItchIo} />
+    el0x.itch.io
+</a>
 
 <style>
     h2 {
-        font-weight: normal;
+        font-size: 2rem;
+        margin-bottom: 0;
     }
 
     a,
     p {
+        font-size: 1.2rem;
+        margin: 1rem;
+
         display: flex;
         align-items: center;
         gap: 0.5rem;
 
         color: light-dark(#222, #ddd);
-    }
 
-    p {
-        margin: 0;
+        transition: color 0.2s ease;
     }
 </style>
